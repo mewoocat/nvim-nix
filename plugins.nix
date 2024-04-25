fe453b64f31c07826c0a36d2d6ad3eb2e861b3c8
mewoocat <azure.horizon.4@gmail.com>
2024-04-24 19:10:38 -0500

    Working on plugins

--- plugins.nix
+++ plugins.nix
@@ -1,12 +1,35 @@
 {pkgs}:
 with pkgs.vimPlugins; [
-      nvim-treesitter.withAllGrammars
-      telescope-nvim
-      bufferline-nvim
-      neo-tree-nvim
-      pywal-nvim
-      wal-vim
-      indent-blankline-nvim
-      neogit
-]
-
+  {
+    plugin = nvim-treesitter.withAllGrammars;
+    config = ""; 
+  }
+  {
+    plugin = telescope-nvim;
+    #config = toLuaFile ./telescope.lua;
+  }
+  {
+    plugin = bufferline-nvim;
+    #config = toLua "require(\"bufferline\").setup()";
+  }
+  {
+    plugin = neo-tree-nvim;
+    config = "";
+  }
+  {
+    plugin = pywal-nvim;
+    config = "";
+  } 
+  {
+    plugin = wal-vim;
+    config = "";
+  } 
+  {
+    plugin = indent-blankline-nvim;
+    #config = toLua "require(\"ibl\").setup()";
+  }
+  {
+    plugin = neogit;
+    config = "";
+  }
+];
